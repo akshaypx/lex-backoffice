5b49c3e036935f14a7beeae8b5aa4b47b6421587
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2023-12-30 01:21:46 +0530

    imagen added

--- src/pages/Gemini/Tab4.tsx
+++ src/pages/Gemini/Tab4.tsx
@@ -1,6 +1,11 @@
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+
+import styles from "./Tab4.module.scss";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Sparkles } from "lucide-react";
+import { Toast, ToastProps } from "primereact/toast";
 
 interface Response {
   predictions?: PredictionsEntity[] | null;
@@ -14,12 +19,23 @@ interface PredictionsEntity {
 const PROJECT_ID = "test1-ywpq";
 //gcloud auth print-access-token
 const ACCESS_TOKEN =
-  "ya29.a0AfB_byBqnwhoK6I9L8jRNlVRTZc7E3gZu0vBMwmSgF1o-x7idW8uTaZhNy13ZVecWPnBnVFG8_8oRids9g09L1aXGdU4RiYe_t8ufp76vRGSEGe3PGFdatP2aIx5mggjL7QW-igYObwpecBdWprQo40_E9yosd3rVweMlbc2Wt8LaCgYKAbMSARESFQHGX2MiD9dcwu-Jr-vJWhXclUjrrw0179";
+  "ya29.a0AfB_byBRia0MllUJy-jODawrRgOcJGgRzIZBeeD3fKz5iD7u3XV7TaU9CWvxtWw1-Dt-x7awpxGfNHJJqtEVLOUPcqN9h9e6oBUTOtOD6r_bSfypam-Fhe6S77rxwQnC2BFuOD5NI3RQTaVkAcSH4OgxszI-kQCX6MFFM99ctsIfaCgYKAdsSARESFQHGX2Mi6w1KO_llNjzxjdckntDF-g0179";
 
 const Tab4 = () => {
   const [prompt, setPrompt] = useState("");
   const [count, setCount] = useState(1);
   const [images, setImages] = useState<string[]>([]);
+  const [isloading, setIsLoading] = useState(false);
+  const toast = useRef<Toast>(null);
+
+  const showToast = () => {
+    toast.current!.show({
+      severity: "info",
+      summary: "Info",
+      detail: "Message Content",
+    });
+  };
+
   const fetchData = async () => {
     try {
       const response = await fetch(
@@ -54,24 +70,42 @@ const Tab4 = () => {
       );
     } catch (err) {
       console.log(err);
+      showToast();
     }
   };
 
   return (
-    <div>
-      <h3>Image Generation using Imaged</h3>
-      <InputText value={prompt} onChange={(e) => setPrompt(e.target.value)} />
-      <Button onClick={() => fetchData()} label="Generate" />
-      <InputText
-        keyfilter="int"
-        placeholder="Integers"
-        max={8}
-        min={1}
-        value={`${count}`}
-        onChange={(e) => setCount(parseInt(e.target.value, 10))}
-      />
+    <div className={styles.container}>
+      <Toast ref={toast} />
+      <h3>Imagen for Image Generation</h3>
+      <div className={styles.actions}>
+        <InputTextarea
+          className={styles.inputarea}
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <div className={styles.actionsInner}>
+          <InputText
+            className={styles.innerInput}
+            keyfilter="int"
+            placeholder="Integers"
+            max={8}
+            min={1}
+            value={`${count}`}
+            onChange={(e) => setCount(parseInt(e.target.value, 10))}
+          />
+          <Button
+            onClick={() => {
+              setIsLoading(true);
+              fetchData().then(() => setIsLoading(false));
+            }}
+            disabled={isloading}
+            icon={isloading ? "pi pi-spin pi-spinner" : <Sparkles />}
+          />
+        </div>
+      </div>
       {/* <button onClick={() => fetchData()}>Fetch</button> */}
-      <div>
+      <div className={styles.gallery}>
         {images.length > 0 &&
           images.map((image, index) => (
             <img
